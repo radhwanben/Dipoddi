8ecba1f83f7013a7dba675b0c0fc7b1234a4f3a5
radhwan ben youssef <radhwanrouihm@gmail.com>
2024-04-02 12:10:50 +0200

    install telegram bundle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" class="hover:text-gray-300">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" class="hover:text-gray-300">Programmes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" class="hover:text-gray-300">Boutique</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
